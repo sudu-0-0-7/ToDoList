6961cb74dafaac0be7b3b20282fa0d4c3b564d42
Mogappu <sudarshana.mogappu@wipfli.com>
2022-11-22 14:56:02 +0530

    updated

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-//window.addEventListener('load', () => {
+const todos=[];
+console.log("line 3",typeof(todos));
+
+console.log("text",localStorage.getItem('todos'));
+
+	//todos.push(localStorage.getItem('todos').split(','));
+	console.log("line 12",typeof(todos));
+
+	console.log("todos value",todos)
+	console.log("ToDo list",typeof(todos));
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
 	const done_el = document.querySelector("#done");
 
-
+	
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
-
-		const task = input.value;
+		const 
+		 task = input.value;
+		
+		console.log(task);
 
         if(input.value=="")
         {
             alert("Please Enter a task")
+			return;
         }
-        else{
-console.log(task);
+
+//console.log(typeof(task));
+			todos.push(task);
+// console.log("Array",todos);
+// console.log(typeof(todos));
+		localStorage.setItem('todos', todos);
+
 		const task_el = document.createElement('div');
 		task_el.classList.add('task');
 
@@ -26,33 +43,27 @@ console.log(task);
 
 		task_el.appendChild(task_content_el);
         
-        
-
 		const task_input_el = document.createElement('input');
 		
-       
 		task_input_el.classList.add('text');
 		task_input_el.type = 'text';
 		task_input_el.value = task;
 		task_input_el.setAttribute('readonly', 'readonly');
-console.log(task_input_el);
+
 		task_content_el.appendChild(task_input_el);
 
 		const task_actions_el = document.createElement('div');
 		task_actions_el.classList.add('actions');
 		
-
         const checkbox_el = document.createElement('input');
         //checkbox_el.setAttribute("type", "checkbox");
 		checkbox_el.type="checkbox";
         task_actions_el.appendChild(checkbox_el);
-        
-	   
+         
 		const task_delete_el = document.createElement('button');
 		task_delete_el.classList.add('delete');
 		task_delete_el.innerText = 'Delete';
-
-        
+ 
         task_actions_el.appendChild(task_delete_el);
 
 		task_el.appendChild(task_actions_el);
@@ -60,75 +71,48 @@ console.log(task_input_el);
 		list_el.appendChild(task_el);
 
 		input.value = '';
+		 
 
-		// checkbox_el.addEventListener('click',function(){
-			
-		// task_content_el.appendChild(task_contents_el);
-	 	// task_el.removeChild();
+		checkbox_el.addEventListener('change', function() {
+		if(this.checked)
+		{
 
-
-		//  })	;
-
-
-		checkbox_el.addEventListener('click',function(){
 			const tasks = task;
-		console.log(tasks);
-		const tasks_el = document.createElement('div');
-		 tasks_el.classList.add('tasks');
-	
-		 const task_contents_el = document.createElement('div');
-		task_contents_el.classList.add('content');
-	console.log(tasks);
-	//console.log(task_contents_el);
-	//list_el.removeChild(task_el);
-	
-		 tasks_el.appendChild(task_contents_el);
-		 
-	
-		 const task_inputs_el = document.createElement('input');
-	
-		 task_inputs_el.classList.add('text');
-		 task_inputs_el.type = 'text';
-		 task_inputs_el.value = tasks;
-		 task_inputs_el.setAttribute('readonly', 'readonly');
-	//console.log(task_input_el);
-		 task_contents_el.appendChild(task_inputs_el);
-		 console.log(task_inputs_el);
-	
-	
-		const task_action_el = document.createElement('div');
-		task_action_el.classList.add('actions');
-		//console.log();
 
-		done_el.appendChild(task_el);
-
-
-	 	task_content_el.classList.toggle('strikeThroughDone');
-
-
+			const tasks_el = document.createElement('div');
+			 tasks_el.classList.add('tasks');
 		
-
-		 
-
-
-
+			 const task_contents_el = document.createElement('div');
+			task_contents_el.classList.add('content');
 		
-		 
+			 tasks_el.appendChild(task_contents_el);
+			 const task_inputs_el = document.createElement('div');
+			 task_inputs_el.classList.add('text');
+			 task_inputs_el.type = 'text';
+			 task_inputs_el.value = tasks;
+			 task_inputs_el.setAttribute('readonly', 'readonly');
+			 task_contents_el.appendChild(task_inputs_el);		
+		
+			const task_action_el = document.createElement('div');
+			task_action_el.classList.add('actions');
+				done_el.appendChild(task_el);
+				task_content_el.classList.toggle('strikeThroughDone');
+		}
+		else
+		{
+			task_actions_el.appendChild(task_delete_el);
+
+			task_delete_el.addEventListener('click', (e) => {
+				done_el.removeChild(task_el);
 		});
-
-		
-
-
-
-
-
+			
+				list_el.appendChild(task_el);
+			task_content_el.classList.toggle('strikeThroughDone');
+			
+		}
+	});	
 		task_delete_el.addEventListener('click', (e) => {
 			list_el.removeChild(task_el);
-		});
-    }
-	
-
-
 	});
-
-//});
+    }
+	);
