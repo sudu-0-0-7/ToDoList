ba98fa85e85e2881b4962bc94f9f2b41a481d479
Mogappu <sudarshana.mogappu@wipfli.com>
2022-11-22 15:10:32 +0530

    updated

--- main.js
+++ main.js
@@ -1,118 +1,117 @@
-const todos=[];
-console.log("line 3",typeof(todos));
+const todos = [];
+console.log("line 3", typeof (todos));
 
-console.log("text",localStorage.getItem('todos'));
+console.log("text", localStorage.getItem('todos'));
 
-	//todos.push(localStorage.getItem('todos').split(','));
-	console.log("line 12",typeof(todos));
+//todos.push(localStorage.getItem('todos').split(','));
+console.log("line 12", typeof (todos));
 
-	console.log("todos value",todos)
-	console.log("ToDo list",typeof(todos));
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
-	const done_el = document.querySelector("#done");
+console.log("todos value", todos)
+console.log("ToDo list", typeof (todos));
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector("#new-task-input");
+const list_el = document.querySelector("#tasks");
+const done_el = document.querySelector("#done");
 
-	
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const 
-		 task = input.value;
-		
-		console.log(task);
 
-        if(input.value=="")
-        {
-            alert("Please Enter a task")
-			return;
-        }
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const task = input.value;
 
-//console.log(typeof(task));
-			todos.push(task);
-// console.log("Array",todos);
-// console.log(typeof(todos));
-		localStorage.setItem('todos', todos);
+	console.log(task);
 
-		const task_el = document.createElement('div');
-		task_el.classList.add('task');
+	if (input.value == "") {
+		alert("Please Enter a task")
+		return;
+	}
 
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
+	//console.log(typeof(task));
+	todos.push(task);
+	// console.log("Array",todos);
+	// console.log(typeof(todos));
+	localStorage.setItem('todos', todos);
+	console.log("local storage",localStorage.getItem('todos'));
 
-		console.log(task_content_el);
 
-		task_el.appendChild(task_content_el);
-        
-		const task_input_el = document.createElement('input');
-		
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
 
-		task_content_el.appendChild(task_input_el);
+	const task_el = document.createElement('div');
+	task_el.classList.add('task');
 
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-		
-        const checkbox_el = document.createElement('input');
-        //checkbox_el.setAttribute("type", "checkbox");
-		checkbox_el.type="checkbox";
-        task_actions_el.appendChild(checkbox_el);
-         
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
- 
-        task_actions_el.appendChild(task_delete_el);
+	const task_content_el = document.createElement('div');
+	task_content_el.classList.add('content');
 
-		task_el.appendChild(task_actions_el);
+	console.log(task_content_el);
 
-		list_el.appendChild(task_el);
+	task_el.appendChild(task_content_el);
 
-		input.value = '';
-		 
+	const task_input_el = document.createElement('input');
 
-		checkbox_el.addEventListener('change', function() {
-		if(this.checked)
-		{
+	task_input_el.classList.add('text');
+	task_input_el.type = 'text';
+	task_input_el.value = task;
+	task_input_el.setAttribute('readonly', 'readonly');
+
+	task_content_el.appendChild(task_input_el);
+
+	const task_actions_el = document.createElement('div');
+	task_actions_el.classList.add('actions');
+
+	const checkbox_el = document.createElement('input');
+	//checkbox_el.setAttribute("type", "checkbox");
+	checkbox_el.type = "checkbox";
+	task_actions_el.appendChild(checkbox_el);
+
+	const task_delete_el = document.createElement('button');
+	task_delete_el.classList.add('delete');
+	task_delete_el.innerText = 'Delete';
+
+	task_actions_el.appendChild(task_delete_el);
+
+	task_el.appendChild(task_actions_el);
+
+	list_el.appendChild(task_el);
+
+	input.value = '';
+
+
+	checkbox_el.addEventListener('change', function () {
+		if (this.checked) {
 
 			const tasks = task;
 
 			const tasks_el = document.createElement('div');
-			 tasks_el.classList.add('tasks');
-		
-			 const task_contents_el = document.createElement('div');
+			tasks_el.classList.add('tasks');
+
+			const task_contents_el = document.createElement('div');
 			task_contents_el.classList.add('content');
-		
-			 tasks_el.appendChild(task_contents_el);
-			 const task_inputs_el = document.createElement('div');
-			 task_inputs_el.classList.add('text');
-			 task_inputs_el.type = 'text';
-			 task_inputs_el.value = tasks;
-			 task_inputs_el.setAttribute('readonly', 'readonly');
-			 task_contents_el.appendChild(task_inputs_el);		
-		
+
+			tasks_el.appendChild(task_contents_el);
+			const task_inputs_el = document.createElement('div');
+			task_inputs_el.classList.add('text');
+			task_inputs_el.type = 'text';
+			task_inputs_el.value = tasks;
+			task_inputs_el.setAttribute('readonly', 'readonly');
+			task_contents_el.appendChild(task_inputs_el);
+
 			const task_action_el = document.createElement('div');
 			task_action_el.classList.add('actions');
-				done_el.appendChild(task_el);
-				task_content_el.classList.toggle('strikeThroughDone');
+			done_el.appendChild(task_el);
+			task_content_el.classList.toggle('strikeThroughDone');
 		}
-		else
-		{
+		else {
 			task_actions_el.appendChild(task_delete_el);
 
 			task_delete_el.addEventListener('click', (e) => {
 				done_el.removeChild(task_el);
-		});
-			
-				list_el.appendChild(task_el);
+			});
+
+			list_el.appendChild(task_el);
 			task_content_el.classList.toggle('strikeThroughDone');
-			
+
 		}
-	});	
-		task_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(task_el);
 	});
-    }
-	);
+	task_delete_el.addEventListener('click', (e) => {
+		list_el.removeChild(task_el);
+	});
+}
+);
